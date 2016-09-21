38131c5d0701a2e61b4a30b79cd9a274a6579d1f
dengfeijie <valer@126.com>
2016-09-21 19:00:31 +0800

    init

--- root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/solr/AclChangeSetsGet.java
+++ root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/solr/AclChangeSetsGet.java
@@ -18,10 +18,6 @@
  */
 package org.alfresco.repo.web.scripts.solr;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.alfresco.repo.solr.AclChangeSet;
 import org.alfresco.repo.solr.SOLRTrackingComponent;
 import org.apache.commons.logging.Log;
@@ -30,59 +26,57 @@ import org.springframework.extensions.webscripts.DeclarativeWebScript;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Support for SOLR: Track ACL Change Sets
  *
  * @since 4.0
  */
-public class AclChangeSetsGet extends DeclarativeWebScript
-{
-    protected static final Log logger = LogFactory.getLog(AclChangeSetsGet.class);
-
-    private SOLRTrackingComponent solrTrackingComponent;
-    
-    public void setSolrTrackingComponent(SOLRTrackingComponent solrTrackingComponent)
-    {
-        this.solrTrackingComponent = solrTrackingComponent;
-    }
-
-    protected Map<String, Object> executeImpl(WebScriptRequest req, Status status)
-    {
-        String fromIdParam = req.getParameter("fromId");
-        String fromTimeParam = req.getParameter("fromTime");
-        String toIdParam = req.getParameter("toId");
-        String toTimeParam = req.getParameter("toTime");
-        String maxResultsParam = req.getParameter("maxResults");
-
-        Long fromId = (fromIdParam == null ? null : Long.valueOf(fromIdParam));
-        Long fromTime = (fromTimeParam == null ? null : Long.valueOf(fromTimeParam));
-        Long toId = (toIdParam == null ? null : Long.valueOf(toIdParam));
-        Long toTime = (toTimeParam == null ? null : Long.valueOf(toTimeParam));
-        int maxResults = (maxResultsParam == null ? 1024 : Integer.valueOf(maxResultsParam));
-        
-        List<AclChangeSet> changesets = solrTrackingComponent.getAclChangeSets(fromId, fromTime, toId, toTime, maxResults);
-        
-        Map<String, Object> model = new HashMap<String, Object>(1, 1.0f);
-        model.put("aclChangeSets", changesets);
-
-        Long maxChangeSetCommitTime = solrTrackingComponent.getMaxChangeSetCommitTime();
-        if(maxChangeSetCommitTime != null)
-        {
-            model.put("maxChangeSetCommitTime", maxChangeSetCommitTime);
-        }
-        
-        Long maxChangeSetId = solrTrackingComponent.getMaxChangeSetId();
-        if(maxChangeSetId != null)
-        {
-            model.put("maxChangeSetId", maxChangeSetId);
-        }
-
-        
-        if (logger.isDebugEnabled())
-        {
-            logger.debug("Result: \n\tRequest: " + req + "\n\tModel: " + model);
-        }
-        
-        return model;
-    }
+public class AclChangeSetsGet extends DeclarativeWebScript {
+	protected static final Log logger = LogFactory.getLog(AclChangeSetsGet.class);
+	
+	private SOLRTrackingComponent solrTrackingComponent;
+	
+	public void setSolrTrackingComponent(SOLRTrackingComponent solrTrackingComponent) {
+		this.solrTrackingComponent = solrTrackingComponent;
+	}
+	
+	protected Map<String, Object> executeImpl(WebScriptRequest req, Status status) {
+		String fromIdParam = req.getParameter("fromId");
+		String fromTimeParam = req.getParameter("fromTime");
+		String toIdParam = req.getParameter("toId");
+		String toTimeParam = req.getParameter("toTime");
+		String maxResultsParam = req.getParameter("maxResults");
+		
+		Long fromId = (fromIdParam == null ? null : Long.valueOf(fromIdParam));
+		Long fromTime = (fromTimeParam == null ? null : Long.valueOf(fromTimeParam));
+		Long toId = (toIdParam == null ? null : Long.valueOf(toIdParam));
+		Long toTime = (toTimeParam == null ? null : Long.valueOf(toTimeParam));
+		int maxResults = (maxResultsParam == null ? 1024 : Integer.valueOf(maxResultsParam));
+		
+		List<AclChangeSet> changesets = solrTrackingComponent.getAclChangeSets(fromId, fromTime, toId, toTime, maxResults);
+		
+		Map<String, Object> model = new HashMap<String, Object>(1, 1.0f);
+		model.put("aclChangeSets", changesets);
+		
+		Long maxChangeSetCommitTime = solrTrackingComponent.getMaxChangeSetCommitTime();
+		if (maxChangeSetCommitTime != null) {
+			model.put("maxChangeSetCommitTime", maxChangeSetCommitTime);
+		}
+		
+		Long maxChangeSetId = solrTrackingComponent.getMaxChangeSetId();
+		if (maxChangeSetId != null) {
+			model.put("maxChangeSetId", maxChangeSetId);
+		}
+		
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Result: \n\tRequest: " + req + "\n\tModel: " + model);
+		}
+		
+		return model;
+	}
 }
--- root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/solr/AclsReadersGet.java
+++ root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/solr/AclsReadersGet.java
@@ -18,12 +18,6 @@
  */
 package org.alfresco.repo.web.scripts.solr;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.alfresco.repo.solr.AclReaders;
 import org.alfresco.repo.solr.SOLRTrackingComponent;
 import org.apache.commons.logging.Log;
@@ -37,83 +31,73 @@ import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptException;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Support for SOLR: Track ACLs
  *
  * @since 4.0
  */
-public class AclsReadersGet extends DeclarativeWebScript
-{
-    protected static final Log logger = LogFactory.getLog(AclsReadersGet.class);
-
-    private SOLRTrackingComponent solrTrackingComponent;
-    
-    public void setSolrTrackingComponent(SOLRTrackingComponent solrTrackingComponent)
-    {
-        this.solrTrackingComponent = solrTrackingComponent;
-    }
-
-    protected Map<String, Object> executeImpl(WebScriptRequest req, Status status)
-    {
-        try
-        {
-            Map<String, Object> model = buildModel(req);
-            if (logger.isDebugEnabled())
-            {
-                logger.debug("Result: \n\tRequest: " + req + "\n\tModel: " + model);
-            }
-            return model;
-        }
-        catch(IOException e)
-        {
-            throw new WebScriptException("IO exception parsing request", e);
-        }
-        catch(JSONException e)
-        {
-            throw new WebScriptException("Invalid JSON", e);
-        }
-    }
-    
-    private Map<String, Object> buildModel(WebScriptRequest req) throws JSONException, IOException
-    {
-        List<Long> aclIds = null;
-        
-        Content content = req.getContent();
-        if (content == null)
-        {
-            throw new WebScriptException("Request content is empty");
-        }
-        JSONObject o = new JSONObject(content.getContent());
-        JSONArray aclIdsJSON = o.has("aclIds") ? o.getJSONArray("aclIds") : null;
-        if (aclIdsJSON == null)
-        {
-            throw new WebScriptException(
-                    Status.STATUS_BAD_REQUEST,
-                    "Parameter 'aclIds' not provided in request content.");
-        }
-        else if (aclIdsJSON.length() == 0 || aclIdsJSON.length() > 512)
-        {
-            throw new WebScriptException(
-                    Status.STATUS_BAD_REQUEST,
-                    "Parameter 'aclIds' must hold from 1 to 512 IDs.");
-        }
-        aclIds = new ArrayList<Long>(aclIdsJSON.length());
-        for (int i = 0; i < aclIdsJSON.length(); i++)
-        {
-            aclIds.add(aclIdsJSON.getLong(i));
-        }
-
-        // Request according to the paging query style required
-        List<AclReaders> aclsReaders = solrTrackingComponent.getAclsReaders(aclIds);
-        
-        Map<String, Object> model = new HashMap<String, Object>(1, 1.0f);
-        model.put("aclsReaders", aclsReaders);
-
-        if (logger.isDebugEnabled())
-        {
-            logger.debug("Result: \n\tRequest: " + req + "\n\tModel: " + model);
-        }
-        
-        return model;
-    }
+public class AclsReadersGet extends DeclarativeWebScript {
+	protected static final Log logger = LogFactory.getLog(AclsReadersGet.class);
+	
+	private SOLRTrackingComponent solrTrackingComponent;
+	
+	public void setSolrTrackingComponent(SOLRTrackingComponent solrTrackingComponent) {
+		this.solrTrackingComponent = solrTrackingComponent;
+	}
+	
+	protected Map<String, Object> executeImpl(WebScriptRequest req, Status status) {
+		try {
+			Map<String, Object> model = buildModel(req);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Result: \n\tRequest: " + req + "\n\tModel: " + model);
+			}
+			return model;
+		} catch (IOException e) {
+			throw new WebScriptException("IO exception parsing request", e);
+		} catch (JSONException e) {
+			throw new WebScriptException("Invalid JSON", e);
+		}
+	}
+	
+	private Map<String, Object> buildModel(WebScriptRequest req) throws JSONException, IOException {
+		List<Long> aclIds = null;
+		
+		Content content = req.getContent();
+		if (content == null) {
+			throw new WebScriptException("Request content is empty");
+		}
+		JSONObject o = new JSONObject(content.getContent());
+		JSONArray aclIdsJSON = o.has("aclIds") ? o.getJSONArray("aclIds") : null;
+		if (aclIdsJSON == null) {
+			throw new WebScriptException(
+					Status.STATUS_BAD_REQUEST,
+					"Parameter 'aclIds' not provided in request content.");
+		} else if (aclIdsJSON.length() == 0 || aclIdsJSON.length() > 512) {
+			throw new WebScriptException(
+					Status.STATUS_BAD_REQUEST,
+					"Parameter 'aclIds' must hold from 1 to 512 IDs.");
+		}
+		aclIds = new ArrayList<Long>(aclIdsJSON.length());
+		for (int i = 0; i < aclIdsJSON.length(); i++) {
+			aclIds.add(aclIdsJSON.getLong(i));
+		}
+		
+		// Request according to the paging query style required
+		List<AclReaders> aclsReaders = solrTrackingComponent.getAclsReaders(aclIds);
+		
+		Map<String, Object> model = new HashMap<String, Object>(1, 1.0f);
+		model.put("aclsReaders", aclsReaders);
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Result: \n\tRequest: " + req + "\n\tModel: " + model);
+		}
+		
+		return model;
+	}
 }
