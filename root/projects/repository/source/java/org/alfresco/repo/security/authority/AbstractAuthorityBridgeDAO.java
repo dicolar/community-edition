4c2a01de3a6f1fd79c56a8a7b53c818698e40abb
dengfeijie <valer@126.com>
2016-09-26 14:51:04 +0800

    fix issues for permission bugs,support sub types of authorityContainer

--- root/projects/repository/source/java/org/alfresco/repo/security/authority/AbstractAuthorityBridgeDAO.java
+++ root/projects/repository/source/java/org/alfresco/repo/security/authority/AbstractAuthorityBridgeDAO.java
@@ -18,156 +18,179 @@
  */
 package org.alfresco.repo.security.authority;
 
-import java.util.List;
-
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.domain.node.NodeDAO;
 import org.alfresco.repo.domain.qname.QNameDAO;
 import org.alfresco.repo.tenant.TenantService;
+import org.alfresco.service.cmr.dictionary.DictionaryService;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.Pair;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Andy
  */
-public abstract class AbstractAuthorityBridgeDAO implements AuthorityBridgeDAO
-{
-
+public abstract class AbstractAuthorityBridgeDAO implements AuthorityBridgeDAO, ApplicationContextAware {
+    
     private NodeDAO nodeDAO;
-
+    
     private QNameDAO qnameDAO;
-
+    
     private TenantService tenantService;
-
+    
+    private ApplicationContext applicationContext;
+    
+    private DictionaryService dictionaryService;
+    
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        this.dictionaryService = applicationContext.getBean("DictionaryService", DictionaryService.class);
+    }
+    
     /**
-     * @param nodeDAO
-     *            the nodeDAO to set
+     * @param nodeDAO the nodeDAO to set
      */
-    public void setNodeDAO(NodeDAO nodeDAO)
-    {
+    public void setNodeDAO(NodeDAO nodeDAO) {
         this.nodeDAO = nodeDAO;
     }
-
+    
     /**
-     * @param qnameDAO
-     *            the qnameDAO to set
+     * @param qnameDAO the qnameDAO to set
      */
-    public void setQnameDAO(QNameDAO qnameDAO)
-    {
+    public void setQnameDAO(QNameDAO qnameDAO) {
         this.qnameDAO = qnameDAO;
     }
-
+    
     /**
-     * @param tenantService
-     *            the tenantService to set
+     * @param tenantService the tenantService to set
      */
-    public void setTenantService(TenantService tenantService)
-    {
+    public void setTenantService(TenantService tenantService) {
         this.tenantService = tenantService;
     }
-
+    
     /*
-     * (non-Javadoc)
-     * @see org.alfresco.repo.security.authority.AuthorityBridgeDAO#getAuthorityBridgeLinks()
-     */
+	 * (non-Javadoc)
+	 * @see org.alfresco.repo.security.authority.AuthorityBridgeDAO#getAuthorityBridgeLinks()
+	 */
     @Override
-    public List<AuthorityBridgeLink> getAuthorityBridgeLinks()
-    {
-        Long authorityContainerTypeQNameId = Long.MIN_VALUE;
-        Pair<Long, QName> authorityContainerTypeQNamePair = qnameDAO.getQName(ContentModel.TYPE_AUTHORITY_CONTAINER);
-        if (authorityContainerTypeQNamePair != null)
-        {
-            authorityContainerTypeQNameId = authorityContainerTypeQNamePair.getFirst();
-        }
-
+    public List<AuthorityBridgeLink> getAuthorityBridgeLinks() {
         Long memberAssocQNameId = Long.MIN_VALUE;
         Pair<Long, QName> memberAssocQNamePair = qnameDAO.getQName(ContentModel.ASSOC_MEMBER);
-        if (memberAssocQNamePair != null)
-        {
+        if (memberAssocQNamePair != null) {
             memberAssocQNameId = memberAssocQNamePair.getFirst();
         }
-
+        
         Long authorityNameQNameId = Long.MIN_VALUE;
         Pair<Long, QName> authorityNameQNamePair = qnameDAO.getQName(ContentModel.PROP_AUTHORITY_NAME);
-        if (authorityNameQNamePair != null)
-        {
+        if (authorityNameQNamePair != null) {
             authorityNameQNameId = authorityNameQNamePair.getFirst();
         }
-
+        
         // Get tenenat specifc store id
         StoreRef tenantSpecificStoreRef = tenantService.getName(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
         Long storeId = Long.MIN_VALUE;
-        if (tenantSpecificStoreRef != null)
-        {
+        if (tenantSpecificStoreRef != null) {
             Pair<Long, StoreRef> storePair = nodeDAO.getStore(tenantSpecificStoreRef);
-            if (storePair != null)
-            {
+            if (storePair != null) {
                 storeId = storePair.getFirst();
             }
         }
-
-        return selectAuthorityBridgeLinks(authorityContainerTypeQNameId, memberAssocQNameId, authorityNameQNameId, storeId);
+        
+        //TODO get sub types' names...
+        Set<QName> set = new HashSet<>();
+        set.add(ContentModel.TYPE_AUTHORITY_CONTAINER);
+        set.addAll(dictionaryService.getSubTypes(ContentModel.TYPE_AUTHORITY_CONTAINER, true));
+        
+        List<AuthorityBridgeLink> result = new ArrayList<>();
+        for (QName type : set) {
+            Long authorityContainerTypeQNameId = Long.MIN_VALUE;
+            Pair<Long, QName> authorityContainerTypeQNamePair = qnameDAO.getQName(type);
+            
+            if (authorityContainerTypeQNamePair != null) {
+                authorityContainerTypeQNameId = authorityContainerTypeQNamePair.getFirst();
+            }
+            
+            if (authorityContainerTypeQNameId == null) continue;
+            
+            result.addAll(selectAuthorityBridgeLinks(authorityContainerTypeQNameId, memberAssocQNameId, authorityNameQNameId, storeId));
+        }
+        
+        return result;
+        
     }
-
     
     
     /* (non-Javadoc)
-     * @see org.alfresco.repo.security.authority.AuthorityBridgeDAO#getDirectAuthoritiesForUser(java.lang.String)
-     */
+	 * @see org.alfresco.repo.security.authority.AuthorityBridgeDAO#getDirectAuthoritiesForUser(java.lang.String)
+	 */
     @Override
-    public List<AuthorityBridgeLink> getDirectAuthoritiesForUser(NodeRef authRef)
-    {
-        Long authorityContainerTypeQNameId = Long.MIN_VALUE;
-        Pair<Long, QName> authorityContainerTypeQNamePair = qnameDAO.getQName(ContentModel.TYPE_AUTHORITY_CONTAINER);
-        if (authorityContainerTypeQNamePair != null)
-        {
-            authorityContainerTypeQNameId = authorityContainerTypeQNamePair.getFirst();
-        }
-
+    public List<AuthorityBridgeLink> getDirectAuthoritiesForUser(NodeRef authRef) {
         Long memberAssocQNameId = Long.MIN_VALUE;
         Pair<Long, QName> memberAssocQNamePair = qnameDAO.getQName(ContentModel.ASSOC_MEMBER);
-        if (memberAssocQNamePair != null)
-        {
+        if (memberAssocQNamePair != null) {
             memberAssocQNameId = memberAssocQNamePair.getFirst();
         }
-
+        
         Long authorityNameQNameId = Long.MIN_VALUE;
         Pair<Long, QName> authorityNameQNamePair = qnameDAO.getQName(ContentModel.PROP_AUTHORITY_NAME);
-        if (authorityNameQNamePair != null)
-        {
+        if (authorityNameQNamePair != null) {
             authorityNameQNameId = authorityNameQNamePair.getFirst();
         }
-
+        
         // Get tenenat specifc store id
         StoreRef tenantSpecificStoreRef = tenantService.getName(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
         Long storeId = Long.MIN_VALUE;
-        if (tenantSpecificStoreRef != null)
-        {
+        if (tenantSpecificStoreRef != null) {
             Pair<Long, StoreRef> storePair = nodeDAO.getStore(tenantSpecificStoreRef);
-            if (storePair != null)
-            {
+            if (storePair != null) {
                 storeId = storePair.getFirst();
             }
         }
-
+        
         Pair<Long, NodeRef> pair = (authRef == null) ? null : nodeDAO.getNodePair(tenantService.getName(authRef));
         
-        return selectDirectAuthoritiesForUser(authorityContainerTypeQNameId, memberAssocQNameId, authorityNameQNameId, storeId, (pair == null) ? -1L : pair.getFirst());
+        //TODO get sub types' names...
+        Set<QName> set = new HashSet<>();
+        set.add(ContentModel.TYPE_AUTHORITY_CONTAINER);
+        set.addAll(dictionaryService.getSubTypes(ContentModel.TYPE_AUTHORITY_CONTAINER, true));
+        
+        List<AuthorityBridgeLink> result = new ArrayList<>();
+        for (QName type : set) {
+            Long authorityContainerTypeQNameId = Long.MIN_VALUE;
+            Pair<Long, QName> authorityContainerTypeQNamePair = qnameDAO.getQName(type);
+            
+            if (authorityContainerTypeQNamePair != null) {
+                authorityContainerTypeQNameId = authorityContainerTypeQNamePair.getFirst();
+            }
+            
+            if (authorityContainerTypeQNameId == null) continue;
+            
+            result.addAll(selectDirectAuthoritiesForUser(authorityContainerTypeQNameId, memberAssocQNameId, authorityNameQNameId, storeId, (pair == null) ? -1L : pair.getFirst()));
+        }
+        
+        return result;
     }
-
+    
     /**
      * @param authorityContainerTypeQNameId
      * @param memberAssocQNameId
      * @param authorityNameQNameId
      * @param storeId
-     * @param user
      * @return
      */
     protected abstract List<AuthorityBridgeLink> selectDirectAuthoritiesForUser(Long authorityContainerTypeQNameId, Long memberAssocQNameId, Long authorityNameQNameId, Long storeId,
-            Long nodeId);
-
+                                                                                Long nodeId);
+    
     /**
      * @param authorityContainerTypeQNameId
      * @param memberAssocQNameId
@@ -176,5 +199,5 @@ public abstract class AbstractAuthorityBridgeDAO implements AuthorityBridgeDAO
      * @return
      */
     protected abstract List<AuthorityBridgeLink> selectAuthorityBridgeLinks(Long authorityContainerTypeQNameId, Long memberAssocQNameId, Long authorityNameQNameId, Long storeId);
-
+    
 }
